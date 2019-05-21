39282911f007e9821d4d40e60fb7f7ca4012f905
ptiforka <limur428@gmail.com>
2019-05-21 21:33:49 +0300

    test auto deploy

--- app/controllers/health_check_controller.rb
+++ app/controllers/health_check_controller.rb
@@ -2,4 +2,8 @@ class HealthCheckController < ApplicationController
   def index
     render json: "Hello World!"
   end
+
+  def some
+    render json: 'test auto deploy'
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
-  scope :api do resources :health_check, only: [:index] end
+  scope :api do
+    resources :health_check, only: [:index]
+    get :some, controller: :health_check
+  end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
